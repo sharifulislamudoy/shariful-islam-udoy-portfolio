5ea281f4f9eeda11452f3044972b767f0283ad76
sharifulislamudoy <surifroton301@gmail.com>
2025-06-28 15:15:14 +0600

    added navbar

---
+++ src/Router/Routes.jsx
@@ -0,0 +1,11 @@
+import {
+  createBrowserRouter
+} from "react-router";
+import MainLayout from "../Layouts/MainLayout";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: MainLayout,
+  },
+]);
