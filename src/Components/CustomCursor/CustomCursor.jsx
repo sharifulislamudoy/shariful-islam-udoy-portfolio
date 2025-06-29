79642e96a09768e2b2f54afbe8822abbcc74b1d4
sharifulislamudoy <surifroton301@gmail.com>
2025-06-29 11:08:53 +0600

    added custome cursore

---
+++ src/Components/CustomCursor/CustomCursor.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+
+const CustomCursor = () => {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const updatePosition = (e) => {
+      setPosition({ x: e.clientX, y: e.clientY });
+    };
+
+    window.addEventListener("mousemove", updatePosition);
+    return () => {
+      window.removeEventListener("mousemove", updatePosition);
+    };
+  }, []);
+
+  return (
+    <div
+      className="custom-cursor"
+      style={{
+        left: `${position.x}px`,
+        top: `${position.y}px`,
+      }}
+    />
+  );
+};
+
+export default CustomCursor;
--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from '../Shared/Navbar';
 import { Outlet } from 'react-router';
+import CustomCursor from '../Components/CustomCursor/CustomCursor';
 
 const MainLayout = () => {
     return (
         <div>
+            <CustomCursor />
             <Navbar />
             <Outlet />
         </div>
